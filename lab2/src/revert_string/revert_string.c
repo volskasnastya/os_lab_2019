4b7e4e107931320ed070afaec2751f7f7b0b1ca5
volskasnastya <nastys.volskas@gmail.com>
2019-03-06 15:38:03 +0000

    lab 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,12 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	char* endPtr = str;
+	while(*(endPtr+1) != '\0')
+	   endPtr++;
+	
+	int boundary = (endPtr-str)%2 == 0 ? (endPtr-str)/2 : (endPtr-str)/2+1; 
+	for(int i = 0; i < boundary; i++)
+	   Swap(endPtr-i,str+i);
 }
 
